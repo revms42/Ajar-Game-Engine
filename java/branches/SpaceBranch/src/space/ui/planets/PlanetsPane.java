7254a91b75ee231193123c200ce403c898fe52b3
revms <38840199+revms42@users.noreply.github.com>
2008-07-02 20:27:46 +0000

    Adding the control panels with their respective packages.

---
+++ java/branches/SpaceBranch/src/space/ui/planets/PlanetsPane.java
@@ -0,0 +1,8 @@
+package space.ui.planets;
+
+import javax.swing.JPanel;
+
+public class PlanetsPane extends JPanel {
+	private static final long serialVersionUID = 4225346196427764883L;
+
+}
---
+++ java/branches/SpaceBranch/src/space/ui/status/StatusPane.java
@@ -0,0 +1,8 @@
+package space.ui.status;
+
+import javax.swing.JPanel;
+
+public class StatusPane extends JPanel {
+	private static final long serialVersionUID = -4985003163537423594L;
+
+}
