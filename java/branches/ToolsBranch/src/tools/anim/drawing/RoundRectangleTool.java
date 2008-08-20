6d4e9887a297ab5d2bb611ac85ee7519b1548f0c
revms <38840199+revms42@users.noreply.github.com>
2008-08-20 20:15:25 +0000

    Created the round rectangle tool.

--- java/branches/ToolsBranch/src/tools/anim/drawing/RoundRectangleTool.java
+++ java/branches/ToolsBranch/src/tools/anim/drawing/RoundRectangleTool.java
@@ -12,7 +12,7 @@ import javax.swing.JPanel;
 import tools.anim.IPaintMode;
 import tools.anim.PaintCanvas;
 
-public class RoundRectangleTool extends AbstractTool<Integer> {
+public class RoundRectangleTool extends AbstractTwoModeTool<Integer> {
 
 	public RoundRectangleTool(PaintCanvas p, IPaintMode t) {
 		super(p, t);
@@ -38,8 +38,8 @@ public class RoundRectangleTool extends AbstractTool<Integer> {
 		if(icon == null){
 			BufferedImage image = new BufferedImage(24, 24, BufferedImage.TYPE_4BYTE_ABGR);
 			Graphics2D g = image.createGraphics();
-			fill(Color.LIGHT_GRAY,g,3,21,18,18,3,3);
-			draw(Color.BLACK,g,3,21,18,18,3,3);
+			fill(Color.LIGHT_GRAY,g,3,3,18,18,3,3);
+			draw(Color.BLACK,g,3,3,18,18,3,3);
 			g.finalize();
 			g.dispose();
 			
@@ -53,5 +53,38 @@ public class RoundRectangleTool extends AbstractTool<Integer> {
 	public JPanel parametricPanel() {
 		// TODO Create Parametric Panel
 		return null;
+	}
+
+	@Override
+	public void handleMouse() {
+		if(!mode2 || startPoint2 == null || endPoint2 == null){
+			paint(
+					this.palette,
+					canvas.getDrawingGraphics(),
+					startPoint.x,
+					startPoint.y,
+					endPoint.x - startPoint.x,
+					endPoint.y - startPoint.y,
+					0,
+					0
+			);
+		}else{
+			paint(
+					this.palette,
+					canvas.getDrawingGraphics(),
+					startPoint.x,
+					startPoint.y,
+					endPoint.x - startPoint.x,
+					endPoint.y - startPoint.y,
+					(int)Math.abs(endPoint2.x-((startPoint.x + endPoint.x)/2)),
+					(int)Math.abs(endPoint2.y-((startPoint.y + endPoint.y)/2))
+	
+			);
+		}
+		
+		if(!inProgress){
+			canvas.pushChange();
+			mode2 = false;
+		}
 	}
 }
