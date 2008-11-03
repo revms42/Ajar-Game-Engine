b6ad8b1f1ab1dfa469429e7980ea7b2eab5c7697
revms <revms@9f04c9cc-e32d-0410-b9a7-b588e17a9f6e>
2008-11-03 23:32:54 +0000

    Set up the ComponentPanel for spaceship creation.

--- java/branches/SpaceBranch/src/space/ui/designer/ComponentPanel.java
+++ java/branches/SpaceBranch/src/space/ui/designer/ComponentPanel.java
@@ -1,19 +1,27 @@
 package space.ui.designer;
 
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import javax.swing.JTree;
 
 public class ComponentPanel extends JPanel {
 	private static final long serialVersionUID = -6642456222626622731L;
 
+	private final JTree componentTree;
 	private final InfoPanel infoPanel;
 	
 	public ComponentPanel(){
 		super();
 		
+		componentTree = new JTree();
 		infoPanel = new InfoPanel();
 		
-		//Component Tree by Type
-		//(^/v scrolling split pane)
-		//Info Panel
+		componentTree.addTreeSelectionListener(infoPanel);
+		
+		JSplitPane split = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
+		
+		split.setTopComponent(new JScrollPane(componentTree));
+		split.setBottomComponent(new JScrollPane(infoPanel));
 	}
 }
