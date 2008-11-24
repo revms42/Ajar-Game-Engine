a3ccca7231fb90d39add19c919ed3c51e478bedf
revms <38840199+revms42@users.noreply.github.com>
2008-11-24 21:05:14 +0000

    Adding names for technology.

--- java/branches/SpaceBranch/src/space/model/tech/Technology.java
+++ java/branches/SpaceBranch/src/space/model/tech/Technology.java
@@ -2,14 +2,14 @@ package space.model.tech;
 
 public enum Technology {
 
-	TECH1("tech1"),
-	TECH2("tech2"),
-	TECH3("tech3"),
-	TECH4("tech4"),
-	TECH5("tech5"),
-	TECH6("tech6"),
-	TECH7("tech7"),
-	TECH8("tech8");
+	TECH1("AppPhys"),
+	TECH2("MatSci"),
+	TECH3("CompSci"),
+	TECH4("MechE"),
+	TECH5("ElecE"),
+	TECH6("ChemE"),
+	TECH7("BioE"),
+	TECH8("XenSci");
 	
 	public final static Technology[] TECHS = {TECH1,TECH2,TECH3,TECH4,TECH5,TECH6,TECH7,TECH8};
 	
