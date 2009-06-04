45ddedd67e592fad4e7617b79320da21ceea0817
revms <revms@9f04c9cc-e32d-0410-b9a7-b588e17a9f6e>
2009-06-04 23:25:56 +0000

    Adding a pathfinding task. Still incomplete, but it functions.

---
+++ java/branches/TopDownTurnBasedBranch/src/strategy/interaction/movement/test/ASharpTest.java
@@ -0,0 +1,63 @@
+package strategy.interaction.movement.test;
+
+import static org.junit.Assert.*;
+
+import java.awt.Point;
+
+import org.IGameManifest;
+import org.junit.Before;
+import org.junit.Test;
+
+import strategy.Strategy;
+import strategy.interaction.movement.ASharp;
+import strategy.model.map.MapStat;
+import strategy.model.map.object.MapObject;
+import strategy.model.map.object.MapObjectStat;
+
+public class ASharpTest {
+
+	public IGameManifest manifest;
+	public ASharp pather;
+	
+	@Before
+	public void setUp() throws Exception {
+		manifest = new Strategy();
+		((Strategy)manifest).start();
+		pather = new ASharp(manifest);
+	}
+
+	@Test
+	public void testFindPath() {
+		Point one = new Point(0,0);
+		Point two = new Point(9,9);
+		
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		boolean result = pather.findPath(one, two);
+		for(Point p : pather.path){
+			MapObject mo = (MapObject)manifest.getEntities(this).toArray()[0];
+			MapObject clone = (MapObject)mo.clone();
+			clone.value(MapObjectStat.MAP_X_POS, p.x * MapObjectStat.TILE_SIZE);
+			clone.value(MapObjectStat.MAP_Y_POS, p.y * MapObjectStat.TILE_SIZE);
+			clone.value(MapObjectStat.MAP_X_DEST, p.x * MapObjectStat.TILE_SIZE);
+			clone.value(MapObjectStat.MAP_Y_DEST, p.y * MapObjectStat.TILE_SIZE);
+			manifest.getEntities(this).add(clone);
+			System.out.println(p);
+		}
+		
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		assertTrue(result);
+	}
+
+}
